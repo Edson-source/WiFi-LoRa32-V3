5250aab99a76350c24f020c556a525bdc20870d4
Edson Ney Martins Júnior <edsonneyjr@gmail.com>
2024-03-23 16:57:02 -0300

    Update Showing Data on Display

--- WiFi_LoRa_32_V3_FactoryTest_Receiver/WiFi_LoRa_32_V3_FactoryTest_Receiver.ino
+++ WiFi_LoRa_32_V3_FactoryTest_Receiver/WiFi_LoRa_32_V3_FactoryTest_Receiver.ino
@@ -28,11 +28,63 @@
 #include "LoRaWan_APP.h"
 #include <Wire.h>  
 #include "HT_SSD1306Wire.h"
+#include <DHT.h>                // Biblioteca DHT
+#include <Adafruit_Sensor.h>
 
-/********************************* WiFi  *********************************************/
-#define SSID "YOUR_SSID"
-#define PASSWORD "YOUR_PASSWORD"
+/* Defines do projeto */
+#define REED          46    // pin onde o sensor magnetico esta conectado
+#define DHTPIN        47    // pin onde dht esta conectado
+#define WindSensor    48    // The pin location of the anemometer sensor
+#define NIVEL         45    // pin onde o sensor de pressão está instalado
+// #define DIRECAO       35    // pin onde o sensor de pressão está instalado
 
+#define DIAMETRO 125       // diametro interno do balde
+#define RAIO     6.25      // raio interno do balde
+#define VOLUME   3.05      // volume da bascula (em cm3) (1cm3 == 1ml) (1ml == 1000mm3)
+
+#define DHTTYPE DHT11 // DHT 11
+DHT dht(DHTPIN, DHTTYPE);
+
+/***************** Variaveis Sensores *****************/
+unsigned long lastSend;
+
+float id = 1;
+
+// Variáveis DHT
+float temperatura_lida = 0;
+float umidade_lida = 0;
+
+// Variáveis pluviometro:
+int val = 0;
+int old_val = 0;
+volatile unsigned long REEDCOUNT = 0;
+float volume_coletado;
+
+// Variaveis Sensor Nivel
+float value = 0;
+float metros = 0;
+
+// Variaveis Anemometro
+volatile unsigned long Rotations = 0; // cup rotation counter used in interrupt routine
+volatile unsigned long ContactBounceTime = 0; // Timer to avoid contact bounce in interrupt routine
+volatile unsigned long ContactBounce = 0;
+
+unsigned long RPM = 0;            //Rotações por minuto
+float speedwind = 0;             //Velocidade do vento (km/h)
+float windspeed = 0;             //Velocidade do vento (m/s)
+
+// --- Constantes ---
+const float pi = 3.14159265;     //Número de pi
+int period = 5000;               //Tempo de medida(miliseconds)
+int delaytime = 2000;            //Invervalo entre as amostras (miliseconds)
+int radius = 120;                //Raio do anemometro(mm)
+
+byte buffer[sizeof(float) * 6];
+/***************** Variaveis Sensores *****************/
+                                                                                      //
+                                                                                      //
+                                                                                      //
+                                                                                      //
 /********************************* lora  *********************************************/
 #define RF_FREQUENCY                                915000000 // Hz
 
@@ -80,14 +132,25 @@ int16_t Rssi,rxSize;
 String rssi = "RSSI --";
 String packSize = "--";
 String packet;
+String packet_t;
+String packet_h;
+String packet_r;
+String packet_w;
 String send_num;
 
+unsigned int Sample = 0;   // Sample number
 unsigned int counter = 0;
 bool receiveflag = false; // software flag for LoRa receiver, received data makes it true.
 long lastSendTime = 0;        // last send time
 int interval = 1000;          // interval between sends
 uint64_t chipid;
 int16_t RssiDetection = 0;
+
+float temperature, humidity, rain, windSpeed, level, id_;
+
+
+SSD1306Wire  factory_display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OLED); // addr , freq , i2c group , resolution , rst
+
 
 // Término do envio de mensagens
 // Muda o estado para Recebedor
@@ -111,13 +174,12 @@ void OnTxTimeout( void )
 // Término recebimento muda estado para Transmissor
 void OnRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr )
 {
-  float temperature, humidity, rain, windSpeed, level, id_;
 
   rxNumber++;
   Rssi=rssi;
   rxSize=size;
 
-  // memcpy(rxpacket, payload, size );
+  memcpy(rxpacket, payload, sizeof(float) );
   memcpy(&temperature, payload, sizeof(float));
   memcpy(&humidity, payload + sizeof(float), sizeof(float));
   memcpy(&rain, payload + sizeof(float) * 2, sizeof(float));
@@ -140,7 +202,6 @@ void OnRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr )
 	receiveflag = true;
   state=STATE_TX;
 }
-
 
 void lora_init(void)
 {
@@ -165,11 +226,12 @@ void lora_init(void)
                                  0, true, 0, 0, LORA_IQ_INVERSION_ON, true );
 	state=STATE_RX;
 }
-
-
 /********************************* lora  *********************************************/
+                                                                                      //
+                                                                                      //
+                                                                                      //
+                                                                                      //
 
-SSD1306Wire  factory_display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OLED); // addr , freq , i2c group , resolution , rst
 
 
 void logo(){
@@ -185,7 +247,7 @@ void WIFISetUp(void)
 	delay(100);
 	WiFi.mode(WIFI_STA);
 	WiFi.setAutoConnect(true);
-	WiFi.begin(SSID,PASSWORD);//fill in "Your WiFi SSID","Your Password"
+	WiFi.begin("Apê 202","COMPIUTERHOUSE");//fill in "Your WiFi SSID","Your Password"
 	delay(100);
 
 	byte count = 0;
@@ -303,6 +365,119 @@ void VextOFF(void) //Vext default OFF
   pinMode(Vext,OUTPUT);
   digitalWrite(Vext, HIGH);
 }
+                                                                                      //
+                                                                                      //
+                                                                                      //
+                                                                                      //
+/********************************* Sensors Functions  *********************************************/
+
+// This is the function that the interrupt calls to increment the rotation count
+void IRAM_ATTR isr_rain () {
+  if ((millis() - ContactBounce) > 50 ) { // debounce the switch contact.
+    REEDCOUNT = REEDCOUNT + 1;              // Adiciona 1 à cntagem de pulsos
+    ContactBounce = millis();
+    // Serial.println("funcao interrupcao chuva");
+  }
+}
+
+void addcount(){
+  counter++;
+} 
+
+// Measure wind speed
+void windvelocity(){
+  speedwind = 0;
+  windspeed = 0;
+  
+  counter = 0;  
+  attachInterrupt(0, addcount, RISING);
+  unsigned long millis();       
+  long startTime = millis();
+  while(millis() < startTime + period) {
+  }
+}
+
+//Função para calcular o RPM
+void RPMcalc() {
+  RPM = ((Rotations) * 60) / (period / 1000); // Calculate revolutions per minute (RPM)
+}
+
+//Velocidade do vento em m/s
+void WindSpeed() {
+  windspeed = ((4 * pi * radius * RPM) / 60) / 1000; //Calcula a velocidade do vento em m/s
+} //end WindSpeed
+
+//Velocidade do vento em km/h
+void SpeedWind() {
+  speedwind = (((4 * pi * radius * RPM) / 60) / 1000) * 3.6; //Calcula velocidade do vento em km/h
+} //end SpeedWind
+
+void get_temp(){
+    temperatura_lida = dht.readTemperature();
+
+    Serial.print("T: ");                    //ESCREVE O TEXTO NO DISPLAY
+    Serial.println(temperatura_lida);
+}
+
+void get_umi(){
+    umidade_lida = dht.readHumidity();  
+
+    Serial.print("U: ");                    //ESCREVE O TEXTO NO DISPLAY   
+    Serial.println(umidade_lida);
+}
+
+void get_rain(){
+    float area_recipiente = 3.14159265 * (RAIO * RAIO); // área da seção transversal do recipiente em cm²
+    float volume_por_virada = (VOLUME/area_recipiente);
+    volume_coletado = (REEDCOUNT * volume_por_virada) * 10; // volume total coletado em cm³
+
+    Serial.print("Viradas: ");
+    Serial.println(REEDCOUNT);
+
+    Serial.print("Chuva: ");
+    Serial.print (volume_coletado);
+    Serial.println(" mm");
+}
+
+void get_wind(){
+    Sample++;
+    Serial.print(Sample);
+    Serial.print(": Start measurement...");
+    windvelocity();
+    Serial.println("   finished.");
+    Serial.print("Counter: ");
+    Serial.print(counter);
+  
+    RPMcalc();
+    Serial.print("RPM: ");
+    Serial.println(RPM);
+
+    WindSpeed();
+    Serial.print("WindSpeed [m/s]: ");
+    Serial.println(windspeed);
+
+    SpeedWind();
+    Serial.print("WindSpeed [km/h]: ");
+    Serial.println(speedwind);
+}
+
+void get_nivel() {
+  value = analogRead(NIVEL);
+  value = (float)value * 5 / 1023;
+
+  Serial.printf("Tensão [V]: "); // Printa no monitor o valor lido pelo sensor
+  Serial.println(val);
+
+  metros = 0.0119*value - 0.2449; //Esse é o algoritmo que fizemos baseado nos dados que obtemos de forma "analogica", anotando os valores das tensões medidadas conforme aumentamos a profundidada do sensor, então essa função não é puramente linear
+  Serial.printf("Metros:");     // Printa o valor convertido em metros
+  Serial.println(metros);
+}
+
+/********************************* Sensors Functions  *********************************************/
+                                                                                      //
+                                                                                      //
+                                                                                      //
+                                                                                      //
 void setup()
 {
 	Serial.begin(115200);
@@ -316,7 +491,7 @@ void setup()
 	factory_display.clear();
 
 	WIFISetUp();
-	WiFi.disconnect(); //
+	WiFi.disconnect(); 
 	WiFi.mode(WIFI_STA);
 	delay(100);
 
@@ -325,6 +500,17 @@ void setup()
 	chipid=ESP.getEfuseMac();//The chip ID is essentially its MAC address(length: 6 bytes).
 	Serial.printf("ESP32ChipID=%04X",(uint16_t)(chipid>>32));//print High 2 bytes
 	Serial.printf("%08X\n",(uint32_t)chipid);//print Low 4bytes.
+
+  /* Inicializa sensor de temperatura e umidade relativa do ar */
+  dht.begin();
+
+  lastSend = 0;
+
+  pinMode(REED, INPUT_PULLUP);
+  pinMode(WindSensor, INPUT);
+  digitalWrite(WindSensor, HIGH);     //internall pull-up active
+  pinMode(NIVEL, INPUT_PULLUP);
+  attachInterrupt(REED, isr_rain, FALLING);
 
 	attachInterrupt(0,interrupt_GPIO0,FALLING);
 	lora_init();
@@ -366,23 +552,31 @@ void loop()
   if(receiveflag && (state==LOWPOWER) )
   {
     receiveflag = false;
-    txNumber++;
-    packet ="R_data:";
+    // packet ="R_data:";
     int i = 0;
-    while(i < rxSize)
-    {
-      packet += rxpacket[i];
-      i++;
-    }
+    // while(i < rxSize)
+    // {
+      // packet += rxpacket[i];
+      packet_t = temperature;
+      packet_h = humidity;
+      packet_r = rain;
+      packet_w = windspeed;
+
+      // packet += humidity;
+      // i++;
+    // }
     packSize = "R_Size: ";
     packSize += String(rxSize,DEC);
     packSize += " R_rssi: ";
     packSize += String(Rssi,DEC);
-    send_num = "send num: ";
-    send_num += String(txNumber,DEC);
-    factory_display.drawString(0, 0, packet);
-    factory_display.drawString(0, 10, packSize);
-    factory_display.drawString(0, 50, send_num);
+    // send_num = "send num: ";
+    // send_num += String(txNumber,DEC);
+    factory_display.drawString(0, 10, packet_t);
+    factory_display.drawString(0, 20, packet_h);
+    factory_display.drawString(0, 30, packet_r);
+    factory_display.drawString(0, 40, packet_w);
+    factory_display.drawString(0, 50, packSize);
+    // factory_display.drawString(0, 50, send_num);
     factory_display.display();
     delay(10);
     factory_display.clear();
@@ -401,10 +595,38 @@ void loop()
       // Serial Monitor Change States
       case STATE_TX:
         delay(1000);
-        txNumber++;
-        sprintf(txpacket,"hello %d,Rssi:%d",txNumber,Rssi);
-        Serial.printf("\r\nsending packet \"%s\" , length %d\r\n",txpacket, strlen(txpacket));
-        Radio.Send( (uint8_t *)txpacket, strlen(txpacket) );
+
+        get_temp();
+        get_umi();
+        get_rain();
+        get_nivel();
+
+        if (millis() - lastSend > 5000){
+          get_wind();
+
+          // Serial.println("Sending packet !!!");
+
+          // send_packets_received();
+          delay(100);
+          
+          // Envio de pacotes através da rede LoraWan
+          txNumber++;
+
+          memcpy(buffer, &temperatura_lida, sizeof(float));
+          memcpy(buffer + sizeof(float), &umidade_lida, sizeof(float));
+          memcpy(buffer + sizeof(float) * 2, &volume_coletado, sizeof(float));
+          memcpy(buffer + sizeof(float) * 3, &windspeed, sizeof(float));
+          memcpy(buffer + sizeof(float) * 4, &metros, sizeof(float));
+          memcpy(buffer + sizeof(float) * 5, &id, sizeof(float));
+
+          // sprintf(txpacket,"hello %d,Rssi:%d",txNumber,Rssi);
+          // Serial.printf("\r\nsending packet \"%s\" , length %d\r\n",buffer, strlen(buffer));
+          Radio.Send( (uint8_t *)buffer, strlen( (char *)buffer) );
+
+          // sendMessage();  // Envio de mensagem através da rede Mesh
+          lastSend = millis();
+        }
+
         state=LOWPOWER;
         break;
       case STATE_RX:
